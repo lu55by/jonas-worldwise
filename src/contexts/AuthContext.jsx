5d1a950b8a34a47da8ec69987380cf34b851886a
lu55by <1738212274@qq.com>
2024-10-14 21:12:45 +0800

    Adding Fake Authentication: Setting Up Context. Changed Login Form's Rows Alignments in Login CSS Module.

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,68 @@
+import { createContext, useContext, useReducer } from "react";
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error(`Unknown action -> ${action.type}!`);
+  }
+};
+
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+const Context = createContext();
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const login = (email, password) => {
+    // Simple logic, change later
+    if (email === FAKE_USER.email && password === FAKE_USER.password) {
+      dispatch({ type: "login", payload: FAKE_USER });
+    } else {
+      // Handle error or display an alert
+      alert("Invalid Authentication. Please try again!");
+    }
+  };
+
+  const logout = () => {
+    dispatch({ type: "logout" });
+  };
+
+  return (
+    <Context.Provider
+      value={{
+        user,
+        isAuthenticated,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+}
+
+function useAuth() {
+  const authContext = useContext(Context);
+  if (authContext === undefined)
+    throw new Error("Context outside of the Provider!");
+  return authContext;
+}
+
+export { AuthProvider, useAuth };
